6ed8580ffcd7bb8733fc9c13d86cffc85a897d92
Mateusz Szczap <mateusz.szczap@gmail.com>
2014-10-23 23:34:09 +0200

    added timeout

--- app/utils/FakeServiceClient.java
+++ app/utils/FakeServiceClient.java
@@ -7,24 +7,26 @@ import play.libs.F;
 import play.libs.Json;
 import play.libs.ws.WS;
 
+import java.util.concurrent.TimeUnit;
+
 public class FakeServiceClient {
 
     public static F.Promise<SearchResults> callSearchResults(final int vehicleId) {
-        return WS.url(createUrl("searchResults", vehicleId)).get()
+        return WS.url(createUrl("searchResults", vehicleId)).setTimeout(timeoutInSecs()).get()
                 .map(response -> response.getBody())
                 .map(str -> Json.fromJson(Json.parse(str), SearchResults.class))
                 .recover(t -> new SearchResults());
     }
 
     public static F.Promise<VehicleData> callVehicleData(final int vehicleId) {
-        return WS.url(createUrl("vehicleData", vehicleId)).get()
+        return WS.url(createUrl("vehicleData", vehicleId)).setTimeout(timeoutInSecs()).get()
                 .map(response -> response.getBody())
                 .map(str -> Json.fromJson(Json.parse(str), VehicleData.class))
                 .recover(t -> new VehicleData());
     }
 
     public static F.Promise<VehicleImage> callVehicleImage(final int vehicleId) {
-        return WS.url(createUrl("vehicleImage", vehicleId)).get()
+        return WS.url(createUrl("vehicleImage", vehicleId)).setTimeout(timeoutInSecs()).get()
                 .map(response -> response.getBody())
                 .map(str -> Json.fromJson(Json.parse(str), VehicleImage.class))
                 .recover(t -> new VehicleImage());
@@ -32,6 +34,10 @@ public class FakeServiceClient {
 
     private static String createUrl(final String name, final int vehicleId) {
         return String.format("http://localhost:9000/mock/%s/%d", name, vehicleId);
+    }
+
+    private static int timeoutInSecs() {
+        return 10 * 1000;
     }
 
 }
